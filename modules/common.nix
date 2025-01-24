1db88ccd63521df9ddf9854059d53acebd94b7bc
jnmcfly <jan.bechtel@gmail.com>
2025-01-24 20:26:07 +0100

    feat: enable EFI support for GRUB and systemd-boot in Nix configuration

--- modules/common.nix
+++ modules/common.nix
@@ -15,9 +15,12 @@
     fsType = "vfat";
   };
 
-  boot.loader.grub.enable = true;
-  boot.loader.grub.devices = [ "/dev/sda" ];
+  boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.efi.efiSysMountPoint = "/boot";
+  boot.loader.grub.devices = [ "/dev/sda" ];
+  boot.loader.grub.efiSupport = true;
+  boot.loader.grub.enable = true;
+  boot.loader.systemd-boot.enable = true;
 
   users.users.jn = {
     isNormalUser = true;
