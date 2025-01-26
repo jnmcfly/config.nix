2f4e50a1276eed398f6e94e236e52a45626df707
jnmcfly <jan.bechtel@gmail.com>
2025-01-26 17:33:01 +0100

    feat: remove package definitions for laptop and desktop from flake.nix

--- flake.nix
+++ flake.nix
@@ -39,12 +39,5 @@
         modules = [ ./home/desktop-home.nix ];
       };
     };
-
-    packages = {
-      x86_64-linux = {
-        laptop = self.nixosConfigurations.laptop.config.system.build.toplevel;
-        desktop = self.nixosConfigurations.desktop.config.system.build.toplevel;
-      };
-    };
   };
 }
