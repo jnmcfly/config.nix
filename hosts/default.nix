629a4e645d3a12ee41be82724d7e279673726a8b
Jan <jan.bechtel@protonmail.com>
2025-07-24 18:52:07 +0200

    Update default.nix

--- hosts/default.nix
+++ hosts/default.nix
@@ -8,6 +8,30 @@
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
+# Aktiviert proprietären NVIDIA-Treiber
+  services.xserver.videoDrivers = [ "nvidia" ];
+
+  hardware.nvidia = {
+    modesetting.enable = true;
+    powerManagement.enable = false;
+    open = false; # false = proprietary, true = open kernel mod (wenn supported)
+    nvidiaSettings = true;
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+  };
+
+  environment.variables = {
+    # Für Wayland mit NVIDIA
+    __GLX_VENDOR_LIBRARY_NAME = "nvidia";
+    GBM_BACKEND = "nvidia-drm";
+    WLR_NO_HARDWARE_CURSORS = "1";
+    WLR_DRM_NO_ATOMIC = "1";
+    LIBVA_DRIVER_NAME = "nvidia";
+    XDG_SESSION_TYPE = "wayland";
+  };
+
+  # Wichtig für Wayland auf NVIDIA
+  boot.kernelParams = [ "nvidia-drm.modeset=1" ];
+
   networking.hostName = "hyprnix";
   networking.networkmanager.enable = true;
 
